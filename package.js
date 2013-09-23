852b82ab3e5882583695afe5921f14c407c014d3
Ubuntu <ubuntu@ip-10-185-4-9.ec2.internal>
2013-09-23 18:55:20 +0000

    commit 2

--- package.js
+++ package.js
@@ -5,7 +5,8 @@ Package.describe({
 Package.on_use(function (api) {
   api.add_files([
   'lib/three.js',
-  'lib/tquery.js',
+	'lib/tquery-bundle-require.js',  
+'lib/tquery.js',
   'lib/physi.js'
   ], 'client'
   );
